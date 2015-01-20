f20f8857af22b6666a052b2baf0222a2f215ad85
rastasheep <rastasheep3@gmail.com>
2015-01-20 19:39:58 +0000

    New stuff, inspired by _why

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,5 +1,12 @@
 alias e="vim"
 
-alias alah="ls -lah"
 alias l="ls -lah"
+alias l.='ls -d .[^.]*'
+alias lsd='ls -ld *(-/DN)'
+alias alah="ls -lah"
+
 alias f="fg"
+alias j="jobs"
+
+alias df="df -hT"
+
