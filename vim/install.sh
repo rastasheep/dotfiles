e8adb196daa75e5def03029f057db7103d4bb9f7
Aleksandar Diklic <rastasheep3@gmail.com>
2016-04-07 22:17:02 +0200

    Switch to neovim

--- vim/install.sh
+++ vim/install.sh
@@ -3,10 +3,12 @@
 # If we're not on a Mac, let's install vim first
 if [ "$(uname -s)" != "Darwin" ]
 then
-  sudo apt-get -q -y install vim
+  yes | sudo add-apt-repository ppa:neovim-ppa/unstable
+  sudo apt-get update 2>&1 1>/dev/null
+  sudo apt-get install neovim
 fi
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-vim +PlugInstall +qall 2>&1 1>/dev/null
+nvim +PlugInstall +qall --headless
---
+++ vim/nvim.conflink/init.vim
@@ -0,0 +1,2 @@
+" I don't like this new neovim convention, just source my existing .vimrc plz
+source ~/.vimrc
