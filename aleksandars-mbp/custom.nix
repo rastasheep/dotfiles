d58a2e56d8afe60c1b2f62faa2f3e305e427ab9e
Aleksandar Diklic <rastasheep3@gmail.com>
2023-06-21 20:15:59 +0200

    Migrate git configs to nix

--- aleksandars-mbp/custom.nix
+++ aleksandars-mbp/custom.nix
@@ -1,5 +1,31 @@
 { pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
+    programs.git = {
+        enable = true;
+        ignores = ["*~" "*.swp" ".DS_Store"];
+        aliases = {
+            a = "add";
+            all = "add -A";
+            st = "status -sb";
+            ci = "commit";
+            ca = "commit --amend";
+            br = "branch";
+            co = "checkout";
+            df = "diff";
+            dfc = "diff --cached";
+            lg = "log --pretty=format:'%C(yellow)%h%Creset %Cgreen(%><(12)%cr%><|(12))%Creset - %s %C(blue)<%an>%Creset'";
+            pl = "pull";
+            ps = "push";
+            undo =  "reset --soft HEAD^";
+            count = "!git shortlog -sne";
+            pr = "!f() { git fetch origin pull/$1/head:pr-$1 && git checkout pr-$1; }; f";
+            up = "!f() { git pull --rebase --prune && git log --pretty=format:\"%Cred%ae %Creset- %C(yellow)%s %Creset(%ar)\" HEAD@{1}.. }; f";
+        };
+    };
 
- 
+    programs.zsh = {
+        shellAliases = {
+            g = "git";
+        };
+    };
 }
--- git/aliases.zsh
+++
@@ -1,7 +0,0 @@
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias grc="git add . && git rebase --continue"
-alias gre="git add . && git reset --hard HEAD"
-
-alias grc="git add . && git rebase --continue"
-alias greset="git add . && git reset --hard HEAD"
-alias gpull="git pull --rebase && git log --color --pretty=oneline --abbrev-commit HEAD@{1}.. | sed 's/^/ /'"
