32ee3f59d86058218379ab55b97eacd749defeb0
Aleksandar Diklic <rastasheep3@gmail.com>
2026-05-06 11:32:55 -0400

    feat(pi): upgrade to v0.73.0
    Upgrades pi-coding-agent from 0.70.2 to 0.73.0 to get the latest features and fixes.

--- packages/pi-coding-agent/default.nix
+++ packages/pi-coding-agent/default.nix
@@ -12,7 +12,7 @@ let
   # Install pi-coding-agent using bun
   piPackage = pkgs.stdenvNoCC.mkDerivation {
     name = "pi-coding-agent";
-    version = "0.68.1";
+    version = "0.73.0";
 
     nativeBuildInputs = [ pkgs.bun pkgs.makeWrapper ];
 
